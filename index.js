f6b93a5499a9227899c8f581e287ac88abe1ef1e
Gregory McWhirter <greg@hallofkvasir.org>
2013-05-22 12:27:27 -0700

    Adding more relic data

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ var optimizer = require("bindings")("swtor_tanking_optimizer.node")
         , stat2: 0 //none
         , rating1: 395
         , rating2: 0
-        , duration: 30
+        , duration: 20
         , cooldown: 120 //confirm?
         }
       , shrouded: {
@@ -72,7 +72,7 @@ var optimizer = require("bindings")("swtor_tanking_optimizer.node")
         , stat2: 3 //absorb
         , rating1: 245
         , rating2: 245
-        , duration: 30
+        , duration: 20
         , cooldown: 120 //confirm?
         }
       }
@@ -101,7 +101,7 @@ var optimizer = require("bindings")("swtor_tanking_optimizer.node")
         , stat2: 0 //none
         , rating1: 425
         , rating2: 0
-        , duration: 30
+        , duration: 20
         , cooldown: 120 //confirm?
         }
       , shrouded: {
@@ -110,7 +110,83 @@ var optimizer = require("bindings")("swtor_tanking_optimizer.node")
         , stat2: 3 //absorb
         , rating1: 265
         , rating2: 265
-        , duration: 30
+        , duration: 20
+        , cooldown: 120 //confirm?
+        }
+      }
+    , dreadguard: {
+        redoubt: {
+          type: 1 //proc
+        , stat: 1 //defense
+        , rating: 455
+        , rate: 0.30
+        , duration: 6
+        , cooldown: 20
+        , can_stack: 0
+        } 
+      , shield_amp: {
+          type: 1 //proc
+        , stat: 3 //absorb
+        , rating: 455
+        , rate: 0.30
+        , duration: 6
+        , cooldown: 20
+        , can_stack: 0
+        }
+      , imperiling: {
+          type: 2 //click
+        , stat1: 1 //defense
+        , stat2: 0 //none
+        , rating1: 350
+        , rating2: 0
+        , duration: 20
+        , cooldown: 120 //confirm?
+        }
+      , shrouded: {
+          type: 2 //click
+        , stat1: 2 //shield
+        , stat2: 3 //absorb
+        , rating1: 215
+        , rating2: 215
+        , duration: 20
+        , cooldown: 120 //confirm?
+        }
+      }
+    , campaign: {
+        redoubt: {
+          type: 1 //proc
+        , stat: 1 //defense
+        , rating: 415 //not verified
+        , rate: 0.30
+        , duration: 6
+        , cooldown: 20
+        , can_stack: 0
+        } 
+      , shield_amp: {
+          type: 1 //proc
+        , stat: 3 //absorb
+        , rating: 415 //not verified
+        , rate: 0.30
+        , duration: 6
+        , cooldown: 20
+        , can_stack: 0
+        }
+      , imperiling: {
+          type: 2 //click
+        , stat1: 1 //defense
+        , stat2: 0 //none
+        , rating1: 315
+        , rating2: 0
+        , duration: 20
+        , cooldown: 120 //confirm?
+        }
+      , shrouded: {
+          type: 2 //click
+        , stat1: 2 //shield
+        , stat2: 3 //absorb
+        , rating1: 190
+        , rating2: 190
+        , duration: 20
         , cooldown: 120 //confirm?
         }
       }
